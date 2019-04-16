30212a2b56585af7f7a43504858941f50127eee0
256hz <a.d@256hz.com>
2019-04-16 16:18:10 -0700

    transitioning to game/round objects

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   get 'games', to: 'games#index'
   get 'games/round', to: 'games#round'
-  post 'games/round', to: 'games#round'
+  post 'games/round', to: 'games#answer'
+  get '/games/game_end', to: 'games#game_end'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_234953) do
+ActiveRecord::Schema.define(version: 2019_04_16_231406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,12 +33,13 @@ ActiveRecord::Schema.define(version: 2019_04_15_234953) do
     t.integer "waste_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "score"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.integer "hood_id"
     t.integer "green_score"
     t.datetime "created_at", null: false
