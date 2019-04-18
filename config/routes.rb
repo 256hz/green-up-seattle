a5a3ea49ea827e536f68f5f4c09810b456a0c40b
jyang81 <joe.yang.design@gmail.com>
2019-04-18 13:27:48 -0700

    edited nav bar, added footer

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   post '/', to: 'sessions#create'
   get '/logout', to: 'sessions#logout'
-
+  get '/resources', to: 'hoods#resources'
   root 'users#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
